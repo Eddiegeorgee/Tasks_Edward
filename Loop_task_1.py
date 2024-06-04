a3c357af9855bdb05ad4b7d79ea8d223256eb215
Retired_Sparrow <157285360+Eddiegeorgee@users.noreply.github.com>
2024-06-04 13:14:29 +0300

    Add files via upload

---
+++ Loop_task_1.py
@@ -0,0 +1,55 @@
+# numbers = [12, 75, 150, 180, 145, 525, 50]
+
+# for number in numbers:
+#     if number > 500:
+#         break
+#     if number > 150:
+#         continue
+#     if number % 5 == 0:
+#         print(number)
+
+#------------------------------------------------------------------------------------------------------------------------------------
+
+# write a program to count the total number of digits in a number using a while loop
+# for example the number is 75869, so the output should be 5
+
+
+#---------------------------------------------------------------------------------------------------------------------------------
+
+# list1=[10,20,30,40,50]
+
+# X=list1[ : : -1]
+# # print(x)
+# for i in X:
+#     print(i)
+
+
+#------------------------------------------------------------------------------------------------------------------------------------
+
+#display numbers from -10 to -1 using loop for
+
+# numbers =[-10,-9,-8,-7,-6,-5,-4,-3,-2,-1]
+
+# x=()
+# for x in numbers:
+#     print(x)
+
+#------------------------------------------------------------------------------------------------------------------------------------
+
+# write a program to print multiplication table of a given number
+# for example, num = 2 
+
+# num = 2
+# for i in range(1, 11):
+#    print(num*i)
+
+
+
+#------------------------------------------------------------------------------------------------------------------------------------
+#display Fibonacci series up to 10 terms
+# the Fibonacci sequence is a series of numbers. the next number is found by adding up the two numbers before it.
+# the first two numbers are 0 and 1.
+#for example, 0,1,1,2,3,5,8,13,21 the next number is this series above is 13+21=34
+
+
+
